3e6813c779c37d0ac71110c3d7aa0d2006f1219f
Steve Conover <sconover@gmail.com>
2010-08-22 15:04:22 -0700

    check the path

--- test/acceptance/url_test.rb
+++ test/acceptance/url_test.rb
@@ -8,19 +8,24 @@ include Pinker
 regarding "a grammar for the structure of a url" do
   
   test "first part of path needs to be a version number" do
-    assert{ grammar.apply_to(URI::parse("/v1?a=1")).well_formed? }
+    assert{ grammar.apply_to(URI::parse("/v1/apples?a=1")).well_formed? }
     assert{ grammar_error("/foo").message == "The first element of the path must be a version number, in the form /vX.  ex: /v2" }
     assert{ grammar_error("/vZZ/foo").message == "The first element of the path must be a version number, in the form /vX.  ex: /v2" }
   end
   
+  test "the second part of the path needs to be a valid resource type" do
+    assert{ grammar.apply_to(URI::parse("/v1/apples?a=1")).well_formed? }
+    assert{ grammar_error("/v1/zzzzz?a=1").message == "The second element of the path must be a resource.  Valid resources are apples, bananas, carrots." }
+  end
+  
   test "'a' is a required query param" do
-    assert{ grammar.apply_to(URI::parse("/v1/foo?a=1")).well_formed? }
-    assert{ grammar_error("/v1/foo").message == "'a' is a required query parameter" }
+    assert{ grammar.apply_to(URI::parse("/v1/apples?a=1")).well_formed? }
+    assert{ grammar_error("/v1/apples").message == "'a' is a required query parameter" }
   end
   
   test "it can only have 'a', 'b', and 'c' query params." do
-    assert{ grammar.apply_to(URI::parse("/v1/foo?a=1&b=2&c=3")).well_formed? }
-    assert{ grammar_error("/v1/foo?x=4&a=1&y=5").message == 
+    assert{ grammar.apply_to(URI::parse("/v1/apples?a=1&b=2&c=3")).well_formed? }
+    assert{ grammar_error("/v1/apples?x=4&a=1&y=5").message == 
               "x, y are not allowed as query parameters.  Valid parameters are: a, b, c." }
   end
   
@@ -36,6 +41,12 @@ regarding "a grammar for the structure of a url" do
           declare("The first element of the path must be a version number, in the form /vX.  ex: /v2"){
             path && path.split("/")[1] =~ /v[0-9]+/
           }
+          
+          declare do |call|
+            allowed_resource_types = %w{apples bananas carrots}
+            path && allowed_resource_types.include?(path.split("/")[2]) || 
+              call.fail("The second element of the path must be a resource.  Valid resources are #{allowed_resource_types.join(', ')}.")
+          end
           
           declare("'a' is a required query parameter"){query && query.include?("a=")}
           
